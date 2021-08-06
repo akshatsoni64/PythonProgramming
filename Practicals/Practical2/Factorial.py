e85baddd512e97bee7c7d6504a9a51390e0da0b4
Akshat Soni <akshatsoni64@gmail.com>
2021-08-06 22:41:44 +0530

    Delete Practicals directory

--- Practicals/Practical2/Factorial.py
+++
@@ -1,6 +0,0 @@
-number = int(input("Enter Number: "))
-fact = 1
-for i in range(1, (number+1)):
-    fact *= i
-
-print(fact)
--- Practicals/Practical2/SeriesSum.py
+++
@@ -1,6 +0,0 @@
-number = int(input("Enter Number Limit: "))
-sum = 0
-for i in range(number+1):
-    sum += i
-
-print(sum)
