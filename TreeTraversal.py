99354d4d3f1f00b2b327636f40f86064404c4c3d
Akshat Soni <akshatsoni64@gmail.com>
2021-08-31 23:01:08 +0530

    Yet to complete the topView of BST

--- TreeTraversal.py
+++ TreeTraversal.py
@@ -4,20 +4,23 @@ class Node:
         self.data = data
         
 class MainClass:
-    def topView(self, root):
-        self.leftView(root.left)
-        print(root.data, end=" ")
-        self.rightView(root.right)
+    m = {}
+    mp = {}
 
-    def leftView(self, root):
+    def verticalOrder(self, root, level):
         if root != None:
-            self.leftView(root.left)
-            print(root.data, end=" ")
-    
-    def rightView(self, root):
+            try:
+                self.mp[level].append(root.data)
+            except:
+                self.mp[level] = [root.data]
+            self.verticalOrder(root.left, level-1)
+            self.verticalOrder(root.right, level+1)
+
+    def topView(self, root, level):
         if root != None:
-            print(root.data, end=" ")
-            self.rightView(root.right)
+            self.m[level] = root.data
+            self.topView(root.left, level-1)
+            self.topView(root.right, level+1)
     
     def height(self, root):
         leftHeight = 0
@@ -84,21 +87,37 @@ class MainClass:
         return
 
 # ip = input("Input Nodes: ")
-# ip = "37 23 108 59 86 64 94 14 105 17 111 65 55 31 79 97 78 25 50 22 66 46 104 98 81 90 68 40 103 77 74 18 69 82 41 4 48 83 67 6 2 95 54 100 99 84 34 88 27 72 32 62 9 56 109 115 33 15 91 29 85 114 112 20 26 30 93 96 87 42 38 60 7 73 35 12 10 57 80 13 52 44 16 70 8 39 107 106 63 24 92 45 75 116 5 61 49 101 71 11 53 43 102 110 1 58 36 28 76 47 113 21 89 51 19 3"
-# vals = list(map(int, ip.split(" ")))
-vals = [6, 3, 5, 4, 7, 2, 1]
+ip = "37 23 108 59 86 64 94 14 105 17 111 65 55 31 79 97 78 25 50 22 66 46 104 98 81 90 68 40 103 77 74 18 69 82 41 4 48 83 67 6 2 95 54 100 99 84 34 88 27 72 32 62 9 56 109 115 33 15 91 29 85 114 112 20 26 30 93 96 87 42 38 60 7 73 35 12 10 57 80 13 52 44 16 70 8 39 107 106 63 24 92 45 75 116 5 61 49 101 71 11 53 43 102 110 1 58 36 28 76 47 113 21 89 51 19 3"
+vals = list(map(int, ip.split(" ")))
+# vals = [6, 3, 5, 4, 7, 2, 1]
 obj = MainClass()
 root = None
 
 for val in vals:
     root = obj.insertNode(root, val)
-print("Tree Top View: ", end="")
-obj.topView(root)
-print("\n\nLevel-order Traversal: ", end="")
-obj.levelOrder(root)
-print("\nIn-order Traversal: ", end="")
-obj.inOrder(root)
-print("\n\nPre-order Traversal: ", end="")
-obj.preOrder(root)
-print("\n\nPost-order Traversal: ", end="")
-obj.postOrder(root)
+
+print("Top View: ")
+obj.topView(root, 0)
+for i in sorted(obj.m):
+    print(obj.m[i], end=" ")
+
+print("\n\nVertical-order Traversal: ")
+obj.verticalOrder(root, 0)
+
+for i in sorted(obj.mp):
+    print(i, "-", obj.mp[i])
+
+# print("Tree Top View: ", end="")
+# obj.topView(root)
+
+# print("\n\nLevel-order Traversal: ", end="")
+# obj.levelOrder(root)
+
+# print("\nIn-order Traversal: ", end="")
+# obj.inOrder(root)
+
+# print("\n\nPre-order Traversal: ", end="")
+# obj.preOrder(root)
+
+# print("\n\nPost-order Traversal: ", end="")
+# obj.postOrder(root)
