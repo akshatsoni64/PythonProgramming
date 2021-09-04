c9f12368e8bae4d5716dffd39867d593596045fa
Akshat Soni <akshatsoni64@gmail.com>
2021-09-04 22:28:07 +0530

    Update TreeTraversal.py

--- TreeTraversal.py
+++ TreeTraversal.py
@@ -6,6 +6,15 @@ class Node:
 class MainClass:
     m = {}
     mp = {}
+    h = 0
+    
+    def rootToLeaf(self, root, level):
+        if root != None:
+            if self.h < level:
+                self.h += 1
+            self.rootToLeaf(root.left, level+1)
+            self.rootToLeaf(root.right, level+1)
+        
 
     def verticalOrder(self, root, level):
         if root != None:
@@ -91,8 +100,9 @@ class MainClass:
         return
 
 # ip = input("Input Nodes: ")
-ip = "37 23 108 59 86 64 94 14 105 17 111 65 55 31 79 97 78 25 50 22 66 46 104 98 81 90 68 40 103 77 74 18 69 82 41 4 48 83 67 6 2 95 54 100 99 84 34 88 27 72 32 62 9 56 109 115 33 15 91 29 85 114 112 20 26 30 93 96 87 42 38 60 7 73 35 12 10 57 80 13 52 44 16 70 8 39 107 106 63 24 92 45 75 116 5 61 49 101 71 11 53 43 102 110 1 58 36 28 76 47 113 21 89 51 19 3"
+# ip = "37 23 108 59 86 64 94 14 105 17 111 65 55 31 79 97 78 25 50 22 66 46 104 98 81 90 68 40 103 77 74 18 69 82 41 4 48 83 67 6 2 95 54 100 99 84 34 88 27 72 32 62 9 56 109 115 33 15 91 29 85 114 112 20 26 30 93 96 87 42 38 60 7 73 35 12 10 57 80 13 52 44 16 70 8 39 107 106 63 24 92 45 75 116 5 61 49 101 71 11 53 43 102 110 1 58 36 28 76 47 113 21 89 51 19 3"
 # ip = "1 2 5 3 4 6"
+ip = "3 5 2 1 4 6 7"
 vals = list(map(int, ip.split(" ")))
 # vals = [6, 3, 5, 4, 7, 2, 1]
 obj = MainClass()
@@ -111,18 +121,22 @@ obj.verticalOrder(root, 0)
 
 for i in sorted(obj.mp):
     print(i, "-", obj.mp[i])
+    
+print("\n\nRoot to Leaf Traversal: ")
+obj.rootToLeaf(root, 0)
+print(obj.h)
 
-# print("Tree Top View: ", end="")
-# obj.topView(root)
+print("Tree Top View: ", end="")
+obj.topView(root)
 
-# print("\n\nLevel-order Traversal: ", end="")
-# obj.levelOrder(root)
+print("\n\nLevel-order Traversal: ", end="")
+obj.levelOrder(root)
 
-# print("\nIn-order Traversal: ", end="")
-# obj.inOrder(root)
+print("\nIn-order Traversal: ", end="")
+obj.inOrder(root)
 
-# print("\n\nPre-order Traversal: ", end="")
-# obj.preOrder(root)
+print("\n\nPre-order Traversal: ", end="")
+obj.preOrder(root)
 
-# print("\n\nPost-order Traversal: ", end="")
-# obj.postOrder(root)
+print("\n\nPost-order Traversal: ", end="")
+obj.postOrder(root)
