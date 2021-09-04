dc0f98f250c6c1c30dd6cdebc0108713d1e51997
Akshat Soni <akshatsoni64@gmail.com>
2021-09-04 11:30:53 +0530

    Update TreeTraversal.py

--- TreeTraversal.py
+++ TreeTraversal.py
@@ -15,10 +15,14 @@ class MainClass:
                 self.mp[level] = [root.data]
             self.verticalOrder(root.left, level-1)
             self.verticalOrder(root.right, level+1)
-
+            
     def topView(self, root, level):
         if root != None:
-            self.m[level] = root.data
+            if level < 0:
+                if level not in self.m.keys():
+                    self.m[level] = root.data
+            else:
+                self.m[level] = root.data
             self.topView(root.left, level-1)
             self.topView(root.right, level+1)
     
@@ -87,7 +91,8 @@ class MainClass:
         return
 
 # ip = input("Input Nodes: ")
-ip = "37 23 108 59 86 64 94 14 105 17 111 65 55 31 79 97 78 25 50 22 66 46 104 98 81 90 68 40 103 77 74 18 69 82 41 4 48 83 67 6 2 95 54 100 99 84 34 88 27 72 32 62 9 56 109 115 33 15 91 29 85 114 112 20 26 30 93 96 87 42 38 60 7 73 35 12 10 57 80 13 52 44 16 70 8 39 107 106 63 24 92 45 75 116 5 61 49 101 71 11 53 43 102 110 1 58 36 28 76 47 113 21 89 51 19 3"
+# ip = "37 23 108 59 86 64 94 14 105 17 111 65 55 31 79 97 78 25 50 22 66 46 104 98 81 90 68 40 103 77 74 18 69 82 41 4 48 83 67 6 2 95 54 100 99 84 34 88 27 72 32 62 9 56 109 115 33 15 91 29 85 114 112 20 26 30 93 96 87 42 38 60 7 73 35 12 10 57 80 13 52 44 16 70 8 39 107 106 63 24 92 45 75 116 5 61 49 101 71 11 53 43 102 110 1 58 36 28 76 47 113 21 89 51 19 3"
+ip = 
 vals = list(map(int, ip.split(" ")))
 # vals = [6, 3, 5, 4, 7, 2, 1]
 obj = MainClass()
