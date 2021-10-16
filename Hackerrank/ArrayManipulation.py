cc7b2dcb8d47d4185465a0fcd17283ac4db6341b
Akshat Soni <akshatsoni64@gmail.com>
2021-10-16 17:51:53 +0530

    Create ArrayManipulation.py

---
+++ Hackerrank/ArrayManipulation.py
@@ -0,0 +1,30 @@
+def arrayManipulation(n, queries):
+    arr = [0]*n
+    for a, b, k in queries:
+        arr[a - 1] += k
+        if b != len(arr):
+            arr[b] -= k
+    maxval = 0
+    itt = 0
+    for q in arr:
+        itt += q
+        if itt > maxval:
+            maxval = itt
+    return maxval
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+    print(result)
