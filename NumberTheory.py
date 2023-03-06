73bec0e726ea5f3ca13dd28769eb755e5341db4a
Akshat Soni <akshatsoni64@gmail.com>
2023-03-06 18:36:00 +0530

    Update NumberTheory.py

--- NumberTheory.py
+++ NumberTheory.py
@@ -13,3 +13,16 @@ def lcm(a, b):
 
 print("GCD:", gcd(5, 10))
 print("\nLCM:", lcm(5, 10))
+
+# 2. Divisors
+from math import sqrt
+def fun1(n):
+    return list(set(x for x in range(1, n+1) if n%x == 0))
+
+def fun2(n):
+    res = []
+    for i in range(1, int(sqrt(n))):
+        res.extend([i, int(n/i)])
+    return res
+ans = fun2(18)
+print(*ans)
